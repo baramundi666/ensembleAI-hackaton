fdfa388ce886966c4ee658c93170657c1e17b0cb
maciejmakowski2003 <maciek2608@onet.pl>
2024-03-16 14:07:06 +0100

    adjust dataset class

--- task-files/modelstealing/load_dataset.py
+++ task-files/modelstealing/load_dataset.py
@@ -3,12 +3,13 @@ from taskdataset import TaskDataset
 from img_augmentation import crop_img, rotate_img, color_distort, add_gaussian_noise
 
 
-if __name__ == "__main__":
-    dataset = torch.load("../modelstealing/data/ModelStealingPub.pt")
-    #dataset = TaskDataset(dataset)
 
-    image = dataset.imgs[3]
-    image = add_gaussian_noise(image)
-    image.show()
+
+if __name__ == "__main__":
+    pt_dataset = torch.load("../modelstealing/data/ModelStealingPub.pt")
+    dataset = TaskDataset()
+    dataset.ids = pt_dataset.ids
+    dataset.imgs = pt_dataset.imgs
+    dataset.labels = pt_dataset.labels
 
     
