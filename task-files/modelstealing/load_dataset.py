e99fca390503429fdba22dea2b81330be839c543
maciejmakowski2003 <maciek2608@onet.pl>
2024-03-16 13:45:41 +0100

    add img_augemnatation file with functio to augmentating images

--- task-files/modelstealing/load_dataset.py
+++ task-files/modelstealing/load_dataset.py
@@ -1,13 +1,14 @@
 import torch
 from taskdataset import TaskDataset
+from img_augmentation import crop_img, rotate_img, color_distort, add_gaussian_noise
 
 
 if __name__ == "__main__":
-    pt_dataset = torch.load("../modelstealing/data/ModelStealingPub.pt")
-    dataset = TaskDataset()
-    dataset.ids = pt_dataset.ids
-    dataset.imgs = pt_dataset.imgs
-    dataset.labels = pt_dataset.labels
-    print(dataset.transform)
-    dataset.imgs[0].show()
-    dataset.imgs[1].show()
+    dataset = torch.load("../modelstealing/data/ModelStealingPub.pt")
+    #dataset = TaskDataset(dataset)
+
+    image = dataset.imgs[3]
+    image = add_gaussian_noise(image)
+    image.show()
+
+    
