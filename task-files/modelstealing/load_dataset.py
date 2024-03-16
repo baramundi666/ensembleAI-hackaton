8aafac93f6610db5b562a3c127862dbf1e877c53
Fiiranek <jellybean@onet.eu>
2024-03-16 12:46:44 +0100

    little change

--- task-files/modelstealing/load_dataset.py
+++ task-files/modelstealing/load_dataset.py
@@ -3,6 +3,7 @@ from taskdataset import TaskDataset
 
 
 if __name__ == "__main__":
-    dataset = torch.load("modelstealing/data/ExampleModelStealingPub.pt")
+    dataset = torch.load("../modelstealing/data/ExampleModelStealingPub.pt")
 
     print(dataset.ids, dataset.imgs, dataset.labels)
+    dataset.imgs[0].show()
