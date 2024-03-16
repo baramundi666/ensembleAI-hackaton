856a6961cb2c43e2aa0bf56c2ffdffaefcb79a0a
maciejmakowski2003 <maciek2608@onet.pl>
2024-03-16 17:00:15 +0100

    fix testing

--- task-files/modelstealing/model_testing.py
+++ task-files/modelstealing/model_testing.py
@@ -27,7 +27,7 @@ transform = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
 
-imgs_tensor = torch.stack(transform(img.convert("RGB")) for img in imgs)
+imgs_tensor = torch.stack([transform(img.convert("RGB")) for img in imgs])
 
 with torch.no_grad():
     predictions = model(imgs_tensor)
