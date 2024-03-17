7726701f33d7169ec34e428decc9fb69bbf5a308
baramundi666 <mateuszkrol221@gmail.com>
2024-03-17 12:15:36 +0100

    refactoring for upload

--- task-files/load_dataset.py
+++
@@ -1,8 +0,0 @@
-import torch
-from taskdataset import TaskDataset
-
-
-if __name__ == "__main__":
-    dataset = torch.load("modelstealing/data/ExampleModelStealingPub.pt")
-
-    print(dataset.ids, dataset.imgs, dataset.labels)
